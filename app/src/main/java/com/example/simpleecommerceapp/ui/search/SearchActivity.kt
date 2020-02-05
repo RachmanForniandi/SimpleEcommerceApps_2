e9ed7f010a5bd7c4ee04b5b009791b834c3c5f43
RachmanForniandi <rachmanforniandi@gmail.com>
2020-02-05 22:55:17 +0700

    search item feature issue done

--- app/src/main/java/com/example/simpleecommerceapp/ui/search/SearchActivity.kt
+++ app/src/main/java/com/example/simpleecommerceapp/ui/search/SearchActivity.kt
@@ -3,11 +3,17 @@ package com.example.simpleecommerceapp.ui.search
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.simpleecommerceapp.R
+import com.example.simpleecommerceapp.ui.cart.CartFragment
 
 class SearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container_search, SearchFragment.newInstance())
+                .commitNow()
+        }
     }
 }
